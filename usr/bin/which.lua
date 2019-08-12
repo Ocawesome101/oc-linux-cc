c2891c308c45ef4e4262b2998824ebe152065bc9
Ocawesome101 <50844998+Ocawesome101@users.noreply.github.com>
2019-08-11 20:39:10 -0400

    Update which.lua

--- usr/bin/which.lua
+++ usr/bin/which.lua
@@ -7,6 +7,9 @@ if args[1] then
  if path:sub(-4) == '.lua' then
   path = path:sub(1,-5)
  end
+ if not path:sub(1,1) == '/' then
+  path = '/' .. path
+ end
  print(path)
 else
  print('Usage: which COMMAND')
