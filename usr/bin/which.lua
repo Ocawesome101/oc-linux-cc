6cca1dd1c537631c09aa15bb420093574b60cb6e
Ocawesome101 <50844998+Ocawesome101@users.noreply.github.com>
2019-10-17 22:45:49 -0400

    Update which.lua

--- usr/bin/which.lua
+++ usr/bin/which.lua
@@ -4,13 +4,17 @@ local args = {...}
 
 if args[1] then
  local path = shell.resolveProgram(args[1])
- if path:sub(-4) == '.lua' then
-  path = path:sub(1,-5)
+ if path and path ~= "" then
+  if path:sub(-4) == '.lua' then
+   path = path:sub(1,-5)
+  end
+  if not path:sub(1,1) == '/' then
+   path = '/' .. path
+  end
+  print(path)
+ else
+  print('which: ' .. args[1] .. ' not found in (' .. shell.path() .. ')')
  end
- if not path:sub(1,1) == '/' then
-  path = '/' .. path
- end
- print(path)
 else
  print('Usage: which COMMAND')
 end
